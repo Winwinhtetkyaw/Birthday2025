f20b4703d492ad8dbf9f6a14b5a1b1c71b5cb929
Win Win Htet Kyaw <winwinhtetkyaw15@gmail.com>
2025-03-03 20:03:30 +0630

    adding animation on html file

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,23 +1,23 @@
-var animationOne = bodymovin.loadAnimation({
-    container: document.getElementById("animation-one"),
-    path: "../assets/animation-json/birthday-animation-one.json",
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-});
+// var animationOne = bodymovin.loadAnimation({
+//     container: document.getElementById("animation-one"),
+//     path: "../animation-json/birthday-animation-one.json",
+//     renderer: "svg",
+//     loop: true,
+//     autoplay: true,
+// });
 
-var animationThree = bodymovin.loadAnimation({
-    container: document.getElementById("animation-three"),
-    path: "/assets/animation-json/birthday-animation-three.json",
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-});
+// var animationThree = bodymovin.loadAnimation({
+//     container: document.getElementById("animation-three"),
+//     path: "/assets/animation-json/birthday-animation-three.json",
+//     renderer: "svg",
+//     loop: true,
+//     autoplay: true,
+// });
 
-var animationFour = bodymovin.loadAnimation({
-    container: document.getElementById("animation-four"),
-    path: "/assets/animation-json/birthday-animation-four.json",
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-});
+// var animationFour = bodymovin.loadAnimation({
+//     container: document.getElementById("animation-four"),
+//     path: "/assets/animation-json/birthday-animation-four.json",
+//     renderer: "svg",
+//     loop: true,
+//     autoplay: true,
+// });
