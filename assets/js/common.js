2dbf8e0c8f413536baf6bfa18811571d41cda6ef
Win Win Htet Kyaw <winwinhtetkyaw15@gmail.com>
2025-03-04 21:41:06 +0630

    adding img scroll frame

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,27 @@
-$(".btn").click(() => {
-    console.log("clicking")
-});
+const scrollers = document.querySelectorAll(".scroller");
+
+
+addAnimation();
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(".scroller_inner");
+        const scrollerContent = Array.from(scrollerInner.children);
+        const duplicates = [];
+
+        for (let i = 0; i < 2; i++) {
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute('aria-hidden', true);
+                duplicates.push(duplicatedItem);
+            });
+        }
+
+        // Append all duplicates to scrollerInner
+        duplicates.forEach((duplicatedItem) => {
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
